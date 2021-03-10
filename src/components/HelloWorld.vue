c52de1b3829c5f8a77769e14c093905b5ef13f1c
five00miles <five00milescn@gmail.com>
2021-03-10 23:26:38 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,59 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <div class="time-item" v-for="(time,idx) in timeList" :key="idx">
+        <el-time-select placeholder="起始时间" v-model="time.startTime"
+                        :picker-options="{start: '00:00',step: '00:01', end: '24:00' }">
+        </el-time-select>
+        <el-time-select placeholder="结束时间" v-model="time.endTime"
+                        :picker-options="{start: '00:00',step: '00:01',end: '24:00',minTime: time.startTime}">
+        </el-time-select>
+      </div>
+    </div>
+
+    <el-button type="primary" @click="handleAdd">增加</el-button>
+    <el-button type="primary" @click="mymethod(timeList)">判断</el-button>
+    <p>{{timeList}}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      timeList: [{ startTime: '', endTime: '' }]
+    }
+  },
+  methods: {
+    handleAdd() {
+      this.timeList.push({ startTime: '', endTime: '' })
+    },
+    mymethod(tableEdit) {
+      var layerEditFlage = false
+      var startTimeArr = []
+      var endTimeArr = []
+      var timeS = ''
+      var timeE = ''
+      for (var i = 0; i < tableEdit.length; i++) {
+        timeS = tableEdit[i].startTime
+        startTimeArr.push(timeS)
+      }
+      for (let i = 0; i < tableEdit.length; i++) {
+        timeE = tableEdit[i].endTime
+        endTimeArr.push(timeE)
+      }
+      var begin = startTimeArr.sort()
+      var over = endTimeArr.sort()
+      for (var k = 1; k < begin.length; k++) {
+        if (begin[k] < over[k - 1]) {
+          console.log(begin[k], over[k - 1])
+          layerEditFlage = true
+        }
+      }
+      if (layerEditFlage) {
+        console.log('时间段有重叠,请检查!!')
+      }
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
